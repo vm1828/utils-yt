be7912831b8fcaf4fb0c65ca187d906c7af6528f
vm1828 <vm18281828@gmail.com>
2024-11-08 15:45:34 +0200

    Update repo

--- downloader.py
+++ downloader.py
@@ -2,7 +2,7 @@ from shared import *
 import csv
 
 if __name__ == '__main__':
-    PLAYLIST = 'https://www.youtube.com/playlist?list=PLaySzQJTCO1lYWD7re003k-Pov6ZynWS-'
+    PLAYLIST = 'https://www.youtube.com/watch?v=WACSnwKby2Y&list=PL8dPuuaLjXtOhFQOVdutRTxvUI1UeCcax&pp=iAQB'
     FILE = 'data/playlist.tsv'
     VIDEO_URL = 'https://youtu.be/J4Wdy0Wc_xQ?list=PLblh5JKOoLUIE96dI3U7oxHaCAbZgfhHk'
     CHANNEL = 'https://www.youtube.com/channel/UCtYLUTtgS3k1Fg4y5tAhLbw/videos'
@@ -10,18 +10,18 @@ if __name__ == '__main__':
     # # get channel url
     # print(get_channel_url(VIDEO_URL))
 
-    # # save data
-    # data = get_playlist(PLAYLIST)
-    # with open(FILE, 'w') as f:
-    #     for url, title in data:
-    #         f.write(f'{url}\t{title}\n')
+    # save data
+    data = get_playlist(PLAYLIST, include_titles=True)
+    with open(FILE, 'w') as f:
+        for url, title in data:
+            f.write(f'{url}\t{title}\n')
 
     # load data from file and download videos
     with open(FILE, 'r') as f:
         data = csv.reader(f, delimiter="\t")
 
-        for i, (url, title) in enumerate(data, start=88):
-            download_video(url, f'{i:03d} - {title}')
-    
-    
-    # download_audio('https://www.youtube.com/watch?v=NMjhKNFE_OA', 'mus1.mp4')
+        # for i, (url, title) in enumerate(data, start=1):
+        #     download_video(url, f'{i:03d} - {title}')
+
+        for (url, title) in data:
+            download_video(url, title)
--- shared.py
+++ shared.py
@@ -1,9 +1,11 @@
 import time
 
-from pytube import YouTube, Playlist, Channel
+import os, subprocess
+from pytubefix import YouTube, Playlist, Channel
+from pytubefix.cli import on_progress
 
-VIDEO_SAVE_DIRECTORY = "./video"
-AUDIO_SAVE_DIRECTORY = "./audio"
+VIDEO_DIR = "./video"
+AUDIO_DIR = "./audio"
 
 # def retry(func):
 #     def wrapper(*args, **kwargs):
@@ -22,21 +24,51 @@ def retry(func):
             try:
                 urls = func(*args, **kwargs)
                 success = True
-            except:
-                print('Failed. Retrying...')
+            except Exception as e:
+                print(f'Failed: {e}. \nRetrying...')
                 time.sleep(0.5)
                 continue
         return urls
     return wrapper
 
+# @retry
+# def download_video(url, filename, high_resolution=True):
+#     video = YouTube(url)
+#     print(url)
+#     print(filename)
+#     video = video.streams.get_highest_resolution() if high_resolution else video.streams.get_lowest_resolution()
+#     video.download(VIDEO_DIR, filename=filename)
+#     print(f"{url} was downloaded successfully")
+
 @retry
-def download_video(url, filename, high_resolution=True):
-    video = YouTube(url)
+def download_video(url, filename):
+    yt = YouTube(url, on_progress_callback = on_progress)
     print(url)
     print(filename)
-    video = video.streams.get_highest_resolution() if high_resolution else video.streams.get_lowest_resolution()
-    video.download(VIDEO_SAVE_DIRECTORY, filename=filename)
-    print(f"{url} was downloaded successfully")
+
+    tmp_video_file = 'video.mp4'
+    tmp_audio_file = 'audio.mp4'
+
+    yt.streams.filter(
+        adaptive=True, file_extension='mp4', only_video=True
+    ).order_by('resolution').desc().first().download(filename=tmp_video_file)
+    yt.streams.filter(
+        adaptive=True, file_extension='mp4', only_audio=True
+    ).order_by('abr').desc().first().download(filename=tmp_audio_file)
+
+    output_file = os.path.join(VIDEO_DIR, filename)
+    cmd = f'ffmpeg -i {tmp_video_file} -i {tmp_audio_file} -c:v copy -c:a aac "{output_file}.mp4"'
+    print(cmd)
+    subprocess.call(cmd, shell=True)  
+    os.remove(tmp_video_file)
+    os.remove(tmp_audio_file)
+
+    # resolutions = ['1080p', '720p', '480p', '360p', '240p']
+    # for res in resolutions:
+    #     for stream in yt.streams:
+    #         if stream.resolution==res:        
+    #             stream.download(VIDEO_DIR, filename=filename)
+    #             return
 
 @retry
 def get_title(video):
@@ -45,14 +77,13 @@ def get_title(video):
 @retry
 def get_playlist(url, include_titles=True):
     playlist = Playlist(url)
-    print(type(playlist))
     # playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
     urls = [url for url in playlist.video_urls]
-    print(urls)
     if include_titles:
-        titles = [get_title(video) for video in playlist.videos]
-        return zip(urls, titles)
-    return urls
+        titles = [f'{i:03d} - {get_title(video)}' for i, video in enumerate(playlist.videos)]
+    else:
+        titles = [f'{i:03d} - {playlist.title}' for i in range(len(urls))]
+    return zip(urls, titles)
 
 @retry
 def get_channel(url, include_titles=True):
@@ -75,5 +106,5 @@ def download_audio(url, filename):
     print('1')
     audio = video.streams.filter(only_audio = True).first()
     print('2')
-    audio.download(AUDIO_SAVE_DIRECTORY, filename=filename)
+    audio.download(AUDIO_DIR, filename=filename)
     print(f"{url} was downloaded successfully")
